0937e631ca1f9fda4451e0835782e03ef7172666
Giuliano Ricca <giulianoricca@icloud.com>
2020-10-16 11:11:14 -0300

    Change static files dirs

--- prayers/settings.py
+++ prayers/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
+    '192.168.100.4',
     'giuliano1004.pythonanywhere.com',
 ]
 
@@ -132,11 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# Add these new lines
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    'prayers/static',
+]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
